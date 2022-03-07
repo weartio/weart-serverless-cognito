9d93333226f17a3a10da17204b47811c576f8e19
mahmJayyab <mahmabujayyab@gmail.com>
2022-03-07 12:25:20 +0300

    modify the get random string function to return random characters and digits

--- src/functions/post_confirmation/core/users/__init__.py
+++ src/functions/post_confirmation/core/users/__init__.py
@@ -20,7 +20,7 @@ def get_random_string(length):
     :param length:
     :return:
     """
-    letters = string.ascii_lowercase
+    letters = string.ascii_lowercase + string.digits
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
