c838d8c11f31f1c69e13a8f51655607ccea331b1
sara diab <sara.emad.diab@gmail.com>
2023-11-06 11:37:48 +0200

    post confirmation

--- src/functions/post_authentication/handler.py
+++ src/functions/post_authentication/handler.py
@@ -9,7 +9,11 @@ def handler(event, context):
     :param context:
     :return:
     """
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    user_attributes['custom:lastLogin'] = current_time
+    try:
+        user_attributes = event['request']['userAttributes']
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        user_attributes['custom:lastLogin'] = current_time
+    except:
+        pass
 
     return event
